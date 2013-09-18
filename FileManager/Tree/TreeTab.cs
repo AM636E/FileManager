9de5f374a6101d61d6aa346f5d2edc5178fa9c55
am636e <bnazariy@gmail.com>
2013-09-18 08:56:08 +0300

    Almost Working

---
+++ FileManager/Tree/TreeTab.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace FileManager
+{
+    public partial class TreeTab : Tab
+    {
+        public TreeTab(string folder) :
+            base(folder) { }
+    }
+}
